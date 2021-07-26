9096e85f03a6b2ffa81516d3a31aefe617120319
dickosmad <dickomoahamed@yahoo.fr>
2021-07-26 15:46:16 +0200

    Mongo schema designing for food ,restaurant,images and user,food,reviews ,order

--- database/image/index.js
+++ database/image/index.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const ImageSchema = new mongoose.Schema({
-  image: [
-    {
-      location: { type: String, required: true },
-    },
-  ],
-});
+const ImageSchema = new mongoose.Schema(
+  {
+    image: [
+      {
+        location: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 export const ImageModel = mongoose.model("Images", ImageSchema);
---
+++ database/menu/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const MenuSchema = new mongoose.Schema(
+  {
+    menu: [
+      {
+        name: { type: String, required: true },
+        items: [{ type: mongoose.Types.ObjectId, ref: "Foods" }],
+      },
+    ],
+    recommended: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Foods",
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const MenuModel = mongoose.model("Menu", MenuSchema);
