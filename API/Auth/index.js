add5fcc126d697964658c896f1804f50f5138e66
dickosmad <dickomoahamed@yahoo.fr>
2021-07-28 16:08:03 +0200

    routes adding signin and signup

--- API/Auth/index.js
+++ API/Auth/index.js
@@ -1,8 +1,6 @@
 import express from "express";
 const Router = express.Router();
 import { UserModel } from "../../database/user";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
 /*
 Route  /signup
@@ -13,25 +11,29 @@ Method  POST
 */
 Router.post("/signup", async (req, res) => {
   try {
-    const { email, password, fullName, phoneNumber } = req.body.credentials;
-    // check if email existed
-    const checkUserByEmail = await UserModel.findOne({ email });
-    const checkUserByPhone = await UserModel.findOne({ phoneNumber });
-    if (checkUserByEmail || checkUserByPhone) {
-      return res.json({ message: "Email already" });
-    }
-    // hash the password
-    const bcryptSalt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(password, bcryptSalt);
-    // Save to the DB
-    await UserModel.create({
-      ...request.body.credentials,
-      password: hashedPassword,
-    });
-    // generate JWT auth token
-    const token = jwt.sign({ user: { fullName, email } }, "Dicko_mohamed");
+    await UserModel.findByEmailAndPhone(req.body.credentials);
+    const newUser = await UserModel.create(req.body.credentials);
+    const token = newUser.generateJwtToken();
+    return res.status(200).json({ token, status: "success" });
   } catch (error) {
-    return res.status(500).json({ message: error });
+    return res.status(500).json({ error: error.message });
+  }
+});
+/*
+Route     /signin
+Des       Signin with email and password
+Params    none
+Access    Public
+Method    POST  
+*/
+Router.post("/signin", async (req, res) => {
+  try {
+    const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+    const token = user.generateJwtToken();
+    console.log(user, token);
+    return res.status(200).json({ token, status: "success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 export default Router;
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+// microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
 let app = express();
 
 // application middleware
@@ -10,7 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 
+// microservices routes
+app.use("/auth", Auth);
+
 app.get("/", function (req, res) {
   res.json({ message: "Success " });
 });
-app.listen(4000, () => console.log("Server is running"));
+app.listen(4000, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
